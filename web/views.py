ecfd27225fabb0f4e12e43f4f31a7cdcd788b4c8
Alfredo Miranda <alfredocdmiranda@gmail.com>
2017-04-13 15:56:33 -0300

    Add a working version of web app

---
+++ web/views.py
@@ -0,0 +1,9 @@
+from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
+
+from web import app
+
+
+@app.route("/")
+def index():
+    view = 'dashboard'
+    return render_template("index.html", view=view)
