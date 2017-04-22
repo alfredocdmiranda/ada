f89fd1bc1c866105f737aad4a27bb56db8049b93
Alfredo Miranda <alfredocdmiranda@gmail.com>
2017-04-22 14:34:57 -0300

    Add mock data and othet stuffs

--- api/views.py
+++ api/views.py
@@ -7,37 +7,71 @@ from api import app, api
 # parser.add_argument('value')
 
 
+MOCK_NODES = [{"id": 1,
+                  "protocol_version": 2.0,
+                  "sketch_version": 0.1,
+                  "sketch_name": "Mock Data 1",
+                  "type": 17,
+                  "battery_level": 100,
+                  "sensors": [{"id": 1,
+                               "description": "Temperature",
+                               "values": [35.0],
+                               "type": 6},
+                              {"id": 2,
+                               "description": "Light",
+                               "values": [1, 6],
+                               "type": 3}]
+                  },
+                 {"id": 2,
+                  "protocol_version": 2.0,
+                  "sketch_version": 0.1,
+                  "sketch_name": "Mock Data 2",
+                  "type": 17,
+                  "battery_level": 12,
+                  "sensors": [{"id": 1,
+                               "description": "Temperature",
+                               "values": [22.0],
+                               "type": 6},
+                              {"id": 2,
+                               "description": "Light",
+                               "values": [1, 6],
+                               "type": 3}]
+                  }]
+
+
 class ApiNodesList(Resource):
     def get(self):
-        nodes = []
+        nodes = MOCK_NODES
 
         return nodes
 
 
 class ApiNode(Resource):
     def get(self, node_id):
-        sensor = {}
-        sensor["id"] = 1
-        sensor["protocol_version"] = '2.0'
-        sensor["sketch_version"] = '0.1'
-        sensor["sketch_name"] = 'Temperature'
-        sensor["type"] = 17
-        sensor["battery_level"] = '80'
-        sensor["children"] = []
-        return sensor
+        if node_id == 1:
+            node = MOCK_NODES[0]
+        else:
+            node = MOCK_NODES[1]
+        return node
 
 
 class ApiSensorsList(Resource):
     def get(self, node_id):
-        children = {}
-        for i in app.gateway.sensors[node_id].children:
-            print(app.gateway.sensors[node_id].children[i].__dict__)
-            children[i] = {}
-            children[i]["id"] = app.gateway.sensors[node_id].children[i].id
-            children[i]["description"] = app.gateway.sensors[node_id].children[i].description
-            children[i]["type"] = app.gateway.sensors[node_id].children[i].type
-            children[i]["values"] = app.gateway.sensors[node_id].children[i].values
-        return children
+        if node_id == 1:
+            node = MOCK_NODES[0]
+        else:
+            node = MOCK_NODES[1]
+
+        # children = []
+        # for i in node['children']:
+        #     # print(app.gateway.sensors[node_id].children[i].__dict__)
+        #     children.append({})
+        #     children[-1]["id"] = app.gateway.sensors[node_id].children[i].id
+        #     children[-1]["description"] = app.gateway.sensors[node_id].children[i].description
+        #     children[-1]["type"] = app.gateway.sensors[node_id].children[i].type
+        #     children[-1]["values"] = app.gateway.sensors[node_id].children[i].values
+
+        return node['children']
 
 
 class ApiSensor(Resource):
