7d787e9aaf89133442c442717e53d0b9f9fefebb
Alfredo Miranda <alfredocdmiranda@gmail.com>
2017-04-22 14:34:22 -0300

    Add Flask-CORS to handle cross domain requests

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+CORS(app)
 
 api = Api(app)
+db = SQLAlchemy(app)
 
 from api import views, models
